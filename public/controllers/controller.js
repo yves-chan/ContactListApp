db75278c8b9ee9b35252c754609c9407add36c5e
yveschan <chanyves@gmail.com>
2015-05-31 11:28:18 -0700

    fixed syntax for controller.js

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,8 +2,11 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
   console.log("Hello World from controller");
 
-  $http.get('/contactlist').success(function(response) {//route that we get data from
+  $http.get('/contactlist').success(function(response) {
+    //route that we get data from
     console.log("I got the data I requested");
-  $scope.contactlist = response;//This makes it so that the controller can be accessed by our view (index.html)
 
-}]);
+    //This makes it so that the controller can be accessed by our view (index.html)
+    $scope.contactlist = response;
+  })}]);
+
