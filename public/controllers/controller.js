7239b7e54da85aa15a9d3d70d4e4c47f6397bff4
yveschan <chanyves@gmail.com>
2015-05-31 11:06:13 -0700

    Made routing requests from server to controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,26 +2,8 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
   console.log("Hello World from controller");
 
-  person1= {
-    name: 'Tim',
-    email: 'tim@gmail.com',
-    number:'(571) 426-1433'
-  };
-
-  person2 = {
-    name:'Liam',
-    email:'neason@taken2.com',
-    number: '(777) 777-7777'
-  };
-
-  person3={
-    name: 'Jessie',
-    email:'jessie@vma.com',
-    number: '(684) 426-1232'
-  };
-
-  var contactlist = [person1, person2, person3];
-
-  $scope.contactlist = contactlist;//This makes it so that the controller can be accessed by our view (index.html)
+  $http.get('/contactlist').success(function(response) {//route that we get data from
+    console.log("I got the data I requested");
+  $scope.contactlist = response;//This makes it so that the controller can be accessed by our view (index.html)
 
 }]);
--- server.js
+++ server.js
@@ -3,8 +3,31 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));//looking through our static files (ie html)
 
+//Tells the server to listen for get request, when it receives, it prints
 app.get('/contactlist', function (req, res) {
   console.log("I received a get request")
+
+  person1= {
+    name: 'Tim',
+    email: 'tim@gmail.com',
+    number:'(571) 426-1433'
+  };
+
+  person2 = {
+    name:'Liam',
+    email:'neason@taken2.com',
+    number: '(777) 777-7777'
+  };
+
+  person3={
+    name: 'Jessie',
+    email:'jessie@vma.com',
+    number: '(684) 426-1232'
+  };
+
+  var contactlist = [person1, person2, person3];
+  res.json(contactlist);//respond by putting data into a json format, which controller can use
+
 });
 
 app.listen(3000);//running on localhost:3000
