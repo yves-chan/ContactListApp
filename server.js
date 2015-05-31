a15e12c92bc306abcef1da18cc92fc72c2d8d72f
yveschan <chanyves@gmail.com>
2015-05-31 10:57:37 -0700

    Linked Controller with view

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));//looking through our static files (ie html)
 
+app.get('/contactlist', function (req, res) {
+  console.log("I received a get request")
+});
+
 app.listen(3000);//running on localhost:3000
 console.log("Server running on port 3000");
