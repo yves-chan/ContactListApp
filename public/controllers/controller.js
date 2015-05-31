b45b349e215864e070c8d5295f13d54305a27ff3
yveschan <chanyves@gmail.com>
2015-05-31 13:58:21 -0700

    Adding contacts not automatically refreshes the page

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,17 +2,29 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
   console.log("Hello World from controller");
 
-  $http.get('/contactlist').success(function(response) {
-    //route that we get data from
-    console.log("I got the data I requested");
+  var refresh = function(){
+    $http.get('/contactlist').success(function(response) {
+      //route that we get data from
+      console.log("I got the data I requested");
 
-    //This makes it so that the controller can be accessed by our view (index.html)
-    $scope.contactlist = response;
-  })
-  
+      //This makes it so that the controller can be accessed by our view (index.html)
+      $scope.contactlist = response;
+      $scope.contact = "";
+    });
+  };
+
+  //intial load of the contacts
+  refresh();
+
   //Adding the contacts to our controller
   $scope.addContact = function() {
     console.log($scope.contact);
+    //sends the input data to server, and add it to db contactlist
+    $http.post('/contactlist',$scope.contact).success(function(response) {
+      console.log(response);
+      //Reload conacts everytime we input more contacts
+      refresh();
+    })
   }
 
 }]);
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');//needs mongojs
 var db = mongojs('contactlist', ['contactlist']);//specify which mongodb we are using, in this case 'contactlist'
-
+var bodyParser = require('body-parser');
 
 
 app.use(express.static(__dirname + "/public"));//looking through our static files (ie html)
+app.use(bodyParser.json());//lets us parse the input data as json
 
 //Tells the server to listen for get request, when it receives, it prints
 app.get('/contactlist', function(req, res) {
@@ -18,6 +19,15 @@ app.get('/contactlist', function(req, res) {
 
   });
 });
+
+//listens to post request from controller, teaches our server how to parse the body of the input data, and add it to database
+app.post('/contactlist', function(req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err,doc) {
+    res.json(doc);
+  });
+});
+
 
 app.listen(3000);//running on localhost:3000
 console.log("Server running on port 3000");
